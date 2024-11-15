58d517d3c66dee0c6d033eb94447da761e75f8b3
Miłosz Skrzypiec <skrzypiec.business@gmail.com>
2024-11-15 22:30:52 +0100

    Create club attack

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,7 @@
 class_name Player
 extends Entity
 
+@export var sword: Weapon
 @export var speed: float
 
 func _ready() -> void:
@@ -12,4 +13,8 @@ func _physics_process(delta: float) -> void:
 	var direction: Vector2 = Vector2(x_direction,y_direction).normalized()
 	
 	velocity = speed * direction
+	
+	if Input.is_action_just_pressed("attack"):
+		sword.attack(position,get_global_mouse_position())
+	
 	move_and_slide()
