e914fb19ee1421ac900c5a4c468648274a1e88e9
Miłosz Skrzypiec <skrzypiec.business@gmail.com>
2024-11-16 16:17:15 +0100

    fix arrow

--- scripts/arrow.gd
+++ scripts/arrow.gd
@@ -4,13 +4,14 @@ extends Attack
 @export var max_duration: float
 
 @onready var duration: float = 0
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
 
 func _physics_process(delta: float) -> void:
 	duration += delta
 	var direction: Vector2 = Vector2(cos(rotation),sin(rotation)).normalized()
 	position += speed * direction * delta
-	if duration > max_duration or targets_hit > 0:
+	if duration > max_duration:
 		queue_free()
+
+func _on_body_entered(body: Node2D) -> void:
+	super(body)
+	queue_free()
