004beddcd8ea2e2049448ae794ee7803c860a38d
wjchswck <wojciechsawicki112358@gmail.com>
2024-11-17 00:42:59 +0100

    added life death loop
    im just like God
    also you cannot loop packedscene references

--- scripts/death_screen.gd
+++ scripts/death_screen.gd
@@ -9,7 +9,7 @@ func _ready():
 	exit_button.button_up.connect(on_exit_button_pressed)
 
 func on_menu_button_pressed() -> void:
-	get_tree().change_scene_to_packed(main_menu)
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
 	
 func on_exit_button_pressed() -> void:
 	get_tree().quit()
